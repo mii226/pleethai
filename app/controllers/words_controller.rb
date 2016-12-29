9124a659d15b5684c3f80ad15bb7f00a4c0de7b4
Kazuki Hirata <family.computer59@gmail.com>
2016-12-29 09:39:53 +0000

    basical function update

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,53 @@
+class WordsController < ApplicationController
+  # 初期表示(TBD:タグとの連携、検索ロジックとの連携)
+   def index
+       @q = Word.ransack(params[:q])
+       @words = @q.result.page(params[:page]).per(10)
+   end
+   # データを閲覧する画面を表示する (TBD:必要か？)
+   def show
+   end
+   
+   # データを作成する画面を表示する
+   def new
+       @word= Word.new
+   end
+   
+   # データを更新する画面を表示する　(TBD:必要か？)
+   def edit
+       @word = Word.find(params[:id])
+   end
+   
+   # データを作成する　(TBD:遷移先、エラー時)
+   def create
+       @word = Word.new(word_params)
+       @word.save
+       redirect_to words_path
+   end
+   
+   # データを更新する　(TBD:遷移先、エラー時、必要か？)
+   def update
+       @word = Word.find(params[:id])
+       @word.update_attributes(word_params)
+       redirect_to words_path
+   end
+   
+   # データを削除する　(TBD:遷移先、エラー時、不要か？)
+   def destroy
+       @word = Word.find(params[:id])
+       @word.destroy
+       redirect_to words_path
+   end
+   
+   # インポート機能 （TBD:必要か？)
+   def add
+    @words = Trancelate.all
+  end
+   
+   
+
+private
+    def word_params
+        params.require(:word).permit(:japanese, :thai, :english, :part_of_speech, :verbal_system, :remarks, :favorits)
+    end
+end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ActiveRecord::Base
-    enum part_of_speech: { Noun: 0, Pronoun: 1, Verb: 2, Adjective: 3, Adverb:4, Preposition:5, Conjunction:6, Interjection:7, Other:9 }
-    enum verbal_system: { Written_language: 0, Spoken_language: 1, Both:9 }
+    enum part_of_speech: { "[N]": 0, "[Pron]": 1, "[V]": 2, "[Adj]": 3, "[Adv]":4, "[Aux]":5, "[Conj]":6, "[Other]":9 }
+    enum verbal_system: { "口語": 0, "文語": 1, "口語/文語":9 }
     acts_as_ordered_taggable_on :tags
 end
