f31deff0966520da119fb0640aba430f1ca39a4e
Kazuki Hirata <family.computer59@gmail.com>
2017-01-19 08:50:02 +0000

    ajast layout

--- app/models/word.rb
+++ app/models/word.rb
@@ -4,5 +4,5 @@ class Word < ActiveRecord::Base
     enum part_of_speech: { "[N]": 0, "[Pron]": 1, "[V]": 2, "[Adj]": 3, "[Adv]":4, "[Aux]":5, "[Conj]":6, "[Other]":9 }
     enum verbal_system: { "口語": 0, "文語": 1, "口語/文語":9 }
     
-    paginates_per 3
+    paginates_per 20
 end
